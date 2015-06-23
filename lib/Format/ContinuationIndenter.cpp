61f2d9fbe2ac23bc714d4cee3692e5d67cbbd997
Alexander Fedorov <fedorov7@gmail.com>
2015-06-23 08:48:55 +0300

    clang-format: revert original code

--- lib/Format/ContinuationIndenter.cpp
+++ lib/Format/ContinuationIndenter.cpp
@@ -126,7 +126,7 @@ bool ContinuationIndenter::canBreak(const LineState &State) {
   // Don't break after very short return types (e.g. "void") as that is often
   // unexpected.
   if (Current.is(TT_FunctionDeclarationName) &&
-      !Style.AlwaysBreakAfterDefinitionReturnType && State.Column < 4)
+      !Style.AlwaysBreakAfterDefinitionReturnType && State.Column < 6)
     return false;
 
   return !State.Stack.back().NoLineBreak;
